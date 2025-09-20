8c3036b6ba43e03ecbdb1906787595279d66b014
Anatole_Gram <tote-anatole-gram@yandex.ru>
2025-09-20 17:51:01 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const FileManagerPlugin = require("filemanager-webpack-plugin");
+
+let mode = process.env.NODE_ENV === 'production' ? 'production' : 'development'
+
+module.exports = {
+    mode,
+    entry: path.join(__dirname, 'app', 'index.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'index.[contenthash].js',
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'app', 'template.html'),
+            filename: 'index.html'
+        }),
+        // заменил на output.clean
+        // new FileManagerPlugin({
+        //     events: {
+        //         onStart: {
+        //             delete: ['dist']
+        //         }
+        //     }
+        // })
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx']  // чтобы не писать расширение при импорте
+    },
+    devtool: 'source-map',
+    devServer: {
+        // watchFiles: path.join(__dirname, 'src'),
+        hot: true,
+        port: 3000
+    }
+}
