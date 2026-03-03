edcc90ec9e12ce024cf02d11c949e66a34487a67
tote <tote-anatole-gram@yandex.ru>
2026-03-03 22:50:16 +0300

    build(webpack): Added headers from devServer.

--- webpack.config.js
+++ webpack.config.js
@@ -88,6 +88,7 @@ module.exports = {
             overlay: true,
             progress: true
         },
+        headers: { "Content-Security-Policy": "default-src 'self'; connect-src 'self' ws: http://localhost:8080; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval';" },
         proxy: [
             {
                 context: ['/api'],
