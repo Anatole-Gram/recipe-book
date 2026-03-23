879bbcb252636d653b9d53b6943281261cee269d
tote <tote-anatole-gram@yandex.ru>
2026-03-23 21:26:28 +0300

    Make the active link highlighted in the navigation menu.

--- app/components/header/headre-nav/HeaderNav.tsx
+++ app/components/header/headre-nav/HeaderNav.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import styles from "./header-nav.module.scss";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 
 export default function HeaderNav() {
 
     return (
         <nav className={styles.nav}>
-            <Link to={`/main`}>главная</Link>
-            <Link to={`/recipe-form`}>новый рецепт</Link>
-            <Link to={`/recipes`}>рецепты</Link>
+
+            <NavLink 
+                to={`/main`} end
+                className={({isActive}) => (isActive ? styles.active : undefined)}>
+                    главная
+            </NavLink>
+
+            <NavLink 
+                to={`/recipe-form`} end
+                className={({isActive}) => (isActive ? styles.active : undefined)}>
+                    новый рецепт
+            </NavLink>
+
+            <NavLink 
+                to={`/recipes`} end
+                className={({isActive}) => (isActive ? styles.active : undefined)}>
+                    рецепты
+            </NavLink>
+
         </nav>
     )
 }
