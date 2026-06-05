d1f5d67ad8e2155ac62345abbd2c2c33bb97ce47
tote <tote-anatole-gram@yandex.ru>
2026-06-05 19:47:12 +0300

    (test): Provide the Jest TextEncoder TextDecoder framework eith access to native Node.js APIs

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleDirectories: ['node_modules', 'src'],
   transform: {
